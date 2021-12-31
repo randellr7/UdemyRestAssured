ec76ea52f60d9b25b25f34c9b73ab979c47b0bc8
Randell <randellr7@gmail.com>
2021-12-30 23:06:03 -0300

    Curso Udemy v1.0

---
+++ src/test/java/rest/S2A3_OlaMundo.java
@@ -0,0 +1,21 @@
+package rest;
+
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+
+public class S2A3_OlaMundo {
+
+    public static void main(String[] args) {
+
+        String url = "https://restapi.wcaquino.me/ola";
+        Response response = RestAssured.request(Method.GET, url);
+
+        System.out.println(response.getBody().asString());
+
+    }
+
+
+
+}
